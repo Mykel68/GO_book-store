23da68eefc9a0760b613c315efa1642500e7b0c6
mykel68 <olowookeremicheal9568@gmail.com>
2024-06-15 12:10:31 +0100

    hi

--- main.go
+++ main.go
@@ -5,4 +5,8 @@ func main() {
 	fmt.Println
 	("Welcome to our my booking application
 	")
+	const TotalTickets = 50
+	var remainingTickets uint = 50
+	fmt.Println("Total tickets", TotalTickets)
+	fmt.Println("Get your tickets here" )
 }

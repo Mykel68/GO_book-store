3cd9b2e3377a89a12cc19ce8c94808789a82e4f5
mykel68 <olowookeremicheal9568@gmail.com>
2024-06-15 12:18:07 +0100

    hii

--- main.go
+++ main.go
@@ -2,11 +2,10 @@ package main
 
 import "fmt"
 func main() {
-	fmt.Println
-	("Welcome to our my booking application
-	")
+	fmt.Print(`Welcome to our my booking application
+	`)
 	const TotalTickets = 50
-	var remainingTickets uint = 50
-	fmt.Println("Total tickets", TotalTickets)
-	fmt.Println("Get your tickets here" )
+
+	fmt.Printf("Total tickets %v\n", TotalTickets)
+
 }

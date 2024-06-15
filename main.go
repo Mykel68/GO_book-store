94ed50820e3df13e38a2e33dcd1e77aa14951cf1
mykel68 <olowookeremicheal9568@gmail.com>
2024-06-15 12:42:08 +0100

    accepting user input

--- main.go
+++ main.go
@@ -8,13 +8,24 @@ func main() {
 
 	fmt.Printf("Total tickets %v\n", TotalTickets)
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
 	// ask user for their name
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
 
-	userName = "Mykel"
-	userTickets = 2
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
 
-	fmt.Printf("User: %v booked %v tickets\n", userName, userTickets)
+	fmt.Println("Enter your email: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+
 
 }
